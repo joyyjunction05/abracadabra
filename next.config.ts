2eb37f99b8fd03d1faebf312a9a831f5db48c632
Vishnu <vishnuvardhan2431s@gmail.com>
2026-04-26 12:32:11 +0530

    mobile ip origin removed

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Authorize mobile hardware testing over local network
-  allowedDevOrigins: ["192.168.1.3", "localhost"],
 };
 
 export default nextConfig;
